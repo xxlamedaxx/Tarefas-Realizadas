dd6b1f864db89d2d08980220f7643a02cd01e09c
xxlamedaxx <leonardo.lameda@sou.inteli.edu.br>
2025-02-21 09:17:55 -0300

    tarefas da aula S3

---
+++ semana3_aula/test.js
@@ -0,0 +1,9 @@
+function verificarParOuImpar(numero) {
+    if (numero % 2 === 0) {
+        console.log(numero + " é par");
+    } else { console.log(numero + " é impar");}
+
+}
+
+verificarParOuImpar(10);
+verificarParOuImpar(7);
---
+++ semana3_aula/test3.js
@@ -0,0 +1,5 @@
+function dobrar(numero){
+    return numero * 2;
+}
+
+console.log(dobrar(5))
